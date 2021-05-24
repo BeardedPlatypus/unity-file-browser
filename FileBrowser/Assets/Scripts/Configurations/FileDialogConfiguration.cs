2fcce43fc025922220301212c8f566d9d8a0e5a8
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-24 23:08:48 +0200

    Add configurations
    - Add abstract DialogConfiguration with common properties
    - Add concrete FileDialogConfiguration
    - Add concrete FolderDialogConfiguration

---
+++ FileBrowser/Assets/Scripts/Configurations/FileDialogConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace FileBrowser.Configurations
+{
+    /// <summary>
+    /// <see cref="FileDialogConfiguration"/> defines the configuration for a
+    /// file dialog.
+    /// </summary>
+    public sealed class FileDialogConfiguration : DialogConfiguration
+    {
+        public override string Title { get; set; } = "File Dialog";
+
+        /// <summary>
+        /// <see cref="FileDialogType"/> defines the type of file dialog.
+        /// </summary>
+        /// <remarks>
+        /// This value will default to Open.
+        /// </remarks>
+        public FileDialogType FileDialogType { get; set; } = FileDialogType.Open;
+
+        /// <summary>
+        /// <see cref="ExtensionFilters"/> defines the extension filters for the
+        /// file dialog.
+        /// </summary>
+        /// <remarks>
+        /// This value will default to all files.
+        /// </remarks>
+        public IReadOnlyList<ExtensionFilter> ExtensionFilters { get; set; } =
+            new[] {ExtensionFilter.Presets.AllFiles()};
+    }
+}
---
+++ FileBrowser/Assets/Scripts/Configurations/FolderDialogConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace FileBrowser.Configurations
+{
+    /// <summary>
+    /// <see cref="FolderDialogConfiguration"/> defines the configuration
+    /// for a folder dialog.
+    /// </summary>
+    public sealed class FolderDialogConfiguration : DialogConfiguration
+    {
+        public override string Title { get; set; } = "Folder Dialog";
+    }
+}
