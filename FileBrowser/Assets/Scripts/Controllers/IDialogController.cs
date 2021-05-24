c9b51f69d1cb8c969b5a83431e7c21031d3b5efc
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-24 23:11:00 +0200

    Add DialogControllerWindows

---
+++ FileBrowser/Assets/Scripts/Controllers/IDialogController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using FileBrowser.Configurations;
+
+namespace FileBrowser.Controllers
+{
+    /// <summary>
+    /// <see cref="IDialogController"/> defines the interface of a file
+    /// browser controller used to open file dialogs.
+    /// </summary>
+    internal interface IDialogController
+    {
+        /// <summary>
+        /// Open a file dialog corresponding with the given <paramref name="configuration"/>
+        /// and return the paths selected by the user.
+        /// </summary>
+        /// <param name="configuration">The configuration of the dialog</param>
+        /// <returns>
+        /// The paths selected by the user. 
+        /// </returns>
+        /// <remarks>
+        /// An empty collection is returned if the action is cancelled.
+        /// </remarks>
+        IEnumerable<string> OpenFileDialog(FileDialogConfiguration configuration);
+
+        /// <summary>
+        /// Open a folder dialog corresponding with the given <paramref name="configuration"/>
+        /// and return the paths selected by the user.
+        /// </summary>
+        /// <param name="configuration">The configuration of the dialog</param>
+        /// <returns>
+        /// The paths selected by the user. 
+        /// </returns>
+        /// <remarks>
+        /// An empty collection is returned if the action is cancelled.
+        /// </remarks>
+        IEnumerable<string> OpenFolderDialog(FolderDialogConfiguration configuration);
+    }
+}
