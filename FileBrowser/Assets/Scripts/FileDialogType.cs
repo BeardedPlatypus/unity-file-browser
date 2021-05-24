e32eeadd5359cac46ae2bc3fa7695bd802852aec
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-24 23:08:09 +0200

    Add FileDialogType

---
+++ FileBrowser/Assets/Scripts/FileDialogType.cs
@@ -0,0 +1,11 @@
+﻿namespace FileBrowser
+{
+    /// <summary>
+    /// <see cref="FileDialogType"/> defines the possible dialog types.
+    /// </summary>
+    public enum FileDialogType
+    {
+        Open,
+        Save,
+    }
+}
